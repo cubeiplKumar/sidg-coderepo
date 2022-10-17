4e2a276d610a5c90ee6feb61751a3e6f26b265bb
cubeiplkumar <emailanilkumarm@gmail.com>
2022-10-17 12:07:03 +0530

    updated Application.php by vivek

--- Application.php
+++ Application.php
@@ -3,3 +3,10 @@
 
 Please make sure all fields are filled in "; echo "and try again.
 \n"; } } docstore.mik.ua/orelly/webprog/php/ch08_05.htm
+
+fetchRow( )){ echo "$row[0]$row[1]$row[2]\n"; } ?> docstore.mik.ua/orelly/webprog/php/ch08_05.htm
+
+
+tr> docstore.mik.ua/orelly/webprog/php/ch08_05.htm
+
+// check for Oracle 8 - data source name syntax is different if ($phptype != 'oci8'){ $dsn = "$phptype://$username:$password@$hostspec/$database"; } else { $net8name = 'www'; $dsn = "$phptype://$username:$password@$net8name"; } // establish the connection $db = DB::connect($dsn); if (DB::isError($db)) { die ($db->getMessage( )); } ?> docstore.mik.ua/orelly/webprog/php/ch08_05.htm
